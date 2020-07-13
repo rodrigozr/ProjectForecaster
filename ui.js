a9ccef4e7840306aee64c369ec02588cef86a8e9
Rodrigo Rosauro <rrosauro@amazon.com>
2020-07-13 18:35:22 -0300

    Delay tooltips a little bit

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip({delay: 500});
 
     function parseSamples(selector) {
         let val = $(selector).val() || '';
